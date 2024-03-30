690e3863348ed916fb19c54e5c10731f8b173ab2
Kosmo K <kosmosworld9@hotmail.com>
2024-03-30 18:22:39 -0400

    Create user and message schemas and install luxon

---
+++ message.js
@@ -0,0 +1,17 @@
+const { DateTime } = require("luxon");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, minLength: 1, maxLength: 100 },
+  time_stamp: { type: Date, required: true },
+  body: { type: String, required: true, minLength: 1, maxLength: 1000 },
+  author: { type: Schema.Types.ObjectId, ref: "Author", required: true },
+});
+
+AuthorSchema.virtual("time_stamp_formatted").get(function () {
+    return this.time_stamp ? DateTime.fromJSDate(this.time_stamp).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
